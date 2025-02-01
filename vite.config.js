79aa2a8a883fd643a81aa2abb2fe7dcdbc1c1013
Arvindsaura <148209983+Arvindsaura@users.noreply.github.com>
2025-02-01 11:56:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // Or your specific base path (e.g., '/my-app/')
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src', // This tells Vite that '@' refers to the 'src' directory
+      '@': '/src',
     },
   },
 });
+
